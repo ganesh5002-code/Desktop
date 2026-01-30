34f4335cf50a0eab6ec8d0fd829a5637dd4545f5
ganesh5002-code <hethaksh.y@gmail.com>
2026-01-30 18:10:57 +0800

    first commit

---
+++ python course/Ai Dev tools/a.py
@@ -0,0 +1,15 @@
+user_name = input("Hello! What is your name?:")
+print("Hello", user_name)
+day = input("how was your day?").lower()
+
+if day =='good':
+    print("That's great to hear! Stay happy and positive!")
+elif day == 'bad': 
+    print("If it helps take some time to rest and think about the positives.")
+else:
+    print("Thanks for letting me know how you feel")
+    
+
+
+
+
